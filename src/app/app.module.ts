5ad5dc9af836cdc649e28cb7a6bb5bf5ed6bb784
Abhishek <choudharyabhishek162@gmail.com>
2022-01-11 13:20:38 +0530

    adding canvas component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MemoryGameComponent } from './memory-game/memory-game.component';
+import { CanvasComponent } from './canvas/canvas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MemoryGameComponent,
+    CanvasComponent
   ],
   imports: [
     BrowserModule,
